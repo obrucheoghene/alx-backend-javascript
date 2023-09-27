5341990ac971d5c846fedd5c9064060506b60dc0
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-27 03:31:12 +0100

    feat: Add  2. Arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,9 @@
-const getNeighborhoodsList = () => {
-  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  // eslint-disable-next-line no-unused-vars
-  const addNeighborhood = (newNeighborhood) => {
-    sanFranciscoNeighborhoods.push(newNeighborhood);
-    return sanFranciscoNeighborhoods;
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
-};
-export default getNeighborhoodsList;
+}
