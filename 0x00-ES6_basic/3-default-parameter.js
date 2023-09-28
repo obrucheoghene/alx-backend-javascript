83ca5bf4f6a624a924ef33e73e447b3d0ab97fbd
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-28 16:12:57 +0100

    feat: Add 3. Parameter defaults

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,3 @@
+// eslint-disable-next-line max-len
+const getSumOfHoods = (initialNumber, expansion1989 = 89, xpansion2019 = 19) => initialNumber + expansion1989 + xpansion2019;
+export default getSumOfHoods;
