9a4f5cdaef47c2f64b669c72c5008402e55006b5
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-29 14:48:17 +0100

    fix: Meet lint requirement

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,4 @@
 // eslint-disable-next-line max-len
-const getSumOfHoods = (initialNumber, expansion1989 = 89, xpansion2019 = 19) => initialNumber + expansion1989 + xpansion2019;
-export default getSumOfHoods;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, xpansion2019 = 19) {
+  return initialNumber + expansion1989 + xpansion2019;
+}
