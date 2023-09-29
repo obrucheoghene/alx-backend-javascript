96ec70747c50a1998f933f6235dfb3140fae63f9
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-29 20:08:13 +0100

    feat: add  3. Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
